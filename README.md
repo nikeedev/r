e463ee3f5b61050ae5d014f99c585435d0982d4a
nikeedev <nikita.gon.dev@gmail.com>
2024-02-20 13:17:47 +0100

    up

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # r
-redirecting 
+
+Redirection system made by nikeedev. 
+
+## Usage:
+
+The basic redirections happen to Github, so you need to use prefixes to redirect to specific websites.
+
+- nikeedev.stio.studio/r/square/clean_square.png will redirect to https://github.com/nikeedev/square/blob/master/clean_square.png 
---
+++ script.js
@@ -0,0 +1,55 @@
+// let pathname = "/r/square/clean_square.png"
+let pathname = window.location.pathname
+
+pathname = pathname.substring(1, pathname.length);
+
+pathname = pathname.split('/');
+
+delete pathname[0];
+
+let repo_name = pathname[1];
+delete pathname[1];
+
+
+pathname = pathname.join("/");
+pathname = pathname.substring(1, pathname.length);
+console.log(pathname);
+
+
+const fileExt = [
+    "html",
+    "css",
+    "js",
+    "json",
+    "xml",
+    "txt",
+    "md",
+    "jpg",
+    "png",
+    "gif",
+    "svg",
+    "pdf",
+    "doc",
+    "docx",
+    "xls",
+    "xlsx",
+    "ppt",
+    "pptx",
+    "zip",
+    "tar",
+    "gz",
+    "mp3",
+    "mp4",
+    "wav",
+    "avi",
+    "mov",
+    "wmv",
+];
+
+fileExt.forEach(a => {
+    if (pathname.endsWith(a)) {
+        window.location.href = `https://github.com/nikeedev/${repo_name}/blob/master${pathname}`;
+    } else {
+        window.location.href = `/`
+    }
+})
